fd4dcbf35f8a1154d2b872f96d10e8a7b12981ca
DWAB Cicconi <danielc@tradecoffeeco.com>
2021-08-17 02:17:18 -0400

    playing with engine

--- js/game.js
+++ js/game.js
@@ -1,58 +1,66 @@
 (function() {
   /** Canvas setup */
-  var canvas = document.getElementById('game');
-  var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('game')
+  var ctx = canvas.getContext('2d')
 
   /** Gameplay Constants */
-  var ALT_BG_COLOR = 'darkmagenta',
-      ALT_LEVEL_COLOR = 'indigo',
-      ARROW_LEFT = 'ArrowLeft',
-      ARROW_RIGHT = 'ArrowRight',
-      DEBUG = 'KeyP',
-      debugMode = 0,
+  var COLORS = {
+    ALT_BG_COLOR: 'darkmagenta',
+    ALT_LEVEL_COLOR: 'indigo',
+    EXIT_COLOR: '#333',
+    LEVEL_COLOR: '#22A9D5',
+    OVERLAP_COLOR: '#f00',
+    PLAYER_COLOR: '#939393'
+  }
+
+  var KEY = {
+    ARROW_LEFT: 'ArrowLeft',
+    ARROW_RIGHT: 'ArrowRight',
+    DEBUG: 'KeyP',
+    SPACE: 'Space'
+  }
+
+  var debugMode = 0,
       ALT_WORLD = 0,
       GRAVITY = 1.1,
       leftPressed = 0,
-      LEVEL_COLOR = '#22A9D5',
-      OVERLAP_COLOR = '#f00',
-      PLAYER_COLOR = '#939393',
       PLAYER_JUMP_SPEED = 6.5,
       PLAYER_MOVE_SPEED = 3,
       PLAYER_SIZE = 20,
       playerX = 0,
       playerY = 300,
+      dx = 0,
+      dy = 0,
+      dt = 0,
       rightPressed = 0,
-      SPACE = 'Space',
       spacePressed = 0
 
-  var LEVEL_1_GEO = [
-    {x:240, y:0, w: PLAYER_SIZE, h: canvas.height}
-  ]
-
-  var LEVEL_1_GEO_ALT = [
-
-  ]
+  var LEVEL_1 = {
+    s: {x:240, y:0},
+    e: {x:430, y:280, w: PLAYER_SIZE, h: PLAYER_SIZE * 2},
+    geo: [{x:240, y:0, w: PLAYER_SIZE, h: canvas.height}],
+    alt: [{x:240, y:canvas.height - canvas.height/4, w: PLAYER_SIZE, h: canvas.height/4}]
+  }
 
   document.addEventListener('keydown', keyDownHandler, false)
   document.addEventListener('keyup', keyUpHandler, false)
 
   function setBackgroundColor(color) {
-    document.body.style.backgroundColor = color ? color : null;
-    document.getElementById('game').style.backgroundColor = color ? ALT_BG_COLOR : null;
+    document.body.style.backgroundColor = color ? color : null
+    document.getElementById('game').style.backgroundColor = color ? COLORS.ALT_BG_COLOR : null
   }
 
   function keyDownHandler(e) {
     if (debugMode) { console.log('key down ', e.code) }
 
-    if (e.code === ARROW_RIGHT) {
-      rightPressed = true
-    } else if (e.code === ARROW_LEFT) {
-      leftPressed = true
-    } else if (e.code === SPACE) {
-      spacePressed = true
-      ALT_WORLD = !ALT_WORLD
-      setBackgroundColor(ALT_WORLD ? ALT_LEVEL_COLOR : null);
-    } else if (e.code === DEBUG) {
+    if (e.code === KEY.ARROW_RIGHT) {
+      rightPressed = 1
+    } else if (e.code === KEY.ARROW_LEFT) {
+      leftPressed = 1
+    } else if (e.code === KEY.SPACE) {
+      spacePressed = 1
+      ALT_WORLD = 1
+    } else if (e.code === KEY.DEBUG) {
       console.log('Debug mode is now ', !debugMode)
       debugMode = !debugMode
     }
@@ -60,37 +68,36 @@
 
   function keyUpHandler(e) {
     if (debugMode) { console.log('key up ', e.code) }
-    if(e.code === ARROW_RIGHT) {
-      rightPressed = false
-    } else if(e.code === ARROW_LEFT) {
-      leftPressed = false
-    } else if (e.code === SPACE) {
-      spacePressed = false
+
+    if (e.code === KEY.ARROW_RIGHT) {
+      rightPressed = 0
+    } else if(e.code === KEY.ARROW_LEFT) {
+      leftPressed = 0
+    } else if (e.code === KEY.SPACE) {
+      spacePressed = 0
+      ALT_WORLD = 0
     }
   }
 
   function gameDebugger() {
     if (debugMode) {
-      ctx.font = '12px Arial';
-      ctx.fillStyle = OVERLAP_COLOR;
-      ctx.fillText('Player: X ' + playerX + ' Y ' + playerY, 20, 32);
+      ctx.font = '12px Arial'
+      ctx.fillStyle = COLORS.OVERLAP_COLOR
+      ctx.fillText('Player: { x:'+playerX+',y:'+playerY+'}',20,32)
+      ctx.fillText('Jump:'+dt,20,52)
     }
   }
 
-  function levelCollisionDetection () {
-    for(var c = 0; c < LEVEL_1_GEO.length; c++) {
-      var geo = LEVEL_1_GEO[c]
-
-      // console.log('playerX + PLAYER_SIZE > geo.x', playerX + PLAYER_SIZE > geo.x)
-      // console.log('playerX + PLAYER_SIZE < geo.x + geo.w', playerX + PLAYER_SIZE < geo.x + geo.w)
-      // console.log('playerY + PLAYER_SIZE > geo.y', playerY + PLAYER_SIZE > geo.y)
-      // console.log('playerY + PLAYER_SIZE < geo.y + geo.h', playerY + PLAYER_SIZE < geo.y + geo.h)
+  function levelCollisionDetection() {
+    var currentLevelGeo = ALT_WORLD ? LEVEL_1.alt : LEVEL_1.geo
+    for(var c = 0; c < currentLevelGeo.length; c++) {
+      var geo = currentLevelGeo[c]
 
       if (
-        (playerX > geo.x) &&
-        (playerX + PLAYER_SIZE < geo.x + geo.w)
-        // (playerY + PLAYER_SIZE < geo.y) &&
-        // (playerY + PLAYER_SIZE > geo.y + geo.h)
+        (playerX + dx + PLAYER_SIZE > geo.x) &&
+        (playerX + dx < geo.x + geo.w) &&
+        (playerY + dy + PLAYER_SIZE > geo.y) &&
+        (playerY + dy < geo.y + geo.h)
       ) {
         return true
       }
@@ -99,78 +106,110 @@
     return false
   }
 
-  function canvasCollisionDetection() {
-    var levelCollision = levelCollisionDetection()
-    console.log('levelCollision', levelCollision)
+  function exitCollisionDetection() {
+    // var exit = currentLevelGeo[e]
+    var exit = LEVEL_1.e
 
-    // if (levelCollision) return
+    if (
+      (playerX + dx + PLAYER_SIZE > exit.x) &&
+      (playerX + dx < exit.x + exit.w) &&
+      (playerY + dy + PLAYER_SIZE > exit.y) &&
+      (playerY + dy < exit.y + exit.h)
+    ) {
+      console.log('load next level')
+    }
+
+    return false
+  }
+
+  function canvasCollisionDetection() {
+    dx = 0
+    dy = 0
 
     if (rightPressed && playerX < canvas.width - PLAYER_SIZE) {
-      playerX += PLAYER_MOVE_SPEED;
+      dx += PLAYER_MOVE_SPEED
     }
 
     if (leftPressed && playerX > 0) {
-      playerX -= PLAYER_MOVE_SPEED;
+      dx -= PLAYER_MOVE_SPEED
     }
 
     /** Make player jump */
-    if (spacePressed && (playerY <= (canvas.height - PLAYER_SIZE)) && (playerY>= 0)) {
-      playerY -= PLAYER_JUMP_SPEED;
+    if (spacePressed && dt <= 25  && (playerY <= (canvas.height - PLAYER_SIZE)) && (playerY>= 0)) {
+      dt += 1
+      dy -= PLAYER_JUMP_SPEED
     } else if (!spacePressed && playerY <= (canvas.height - PLAYER_SIZE)) {
-      if (playerY + PLAYER_JUMP_SPEED >= (canvas.height - PLAYER_SIZE)) {
-        playerY += ((canvas.height - PLAYER_SIZE) - playerY)
+      dt = 0
 
+      if (playerY + PLAYER_JUMP_SPEED >= (canvas.height - PLAYER_SIZE)) {
+        dy += ((canvas.height - PLAYER_SIZE) - playerY)
       } else {
-        playerY += (PLAYER_JUMP_SPEED * 1.1)
+        dy += (PLAYER_JUMP_SPEED)
       }
+    } else if ( playerY + PLAYER_SIZE <= canvas.height ) {
+      dy += (PLAYER_JUMP_SPEED)
     }
+
+    var cc = levelCollisionDetection()
+    if (dx !== 0 && !cc) playerX += dx
+    if (dy !== 0 && !cc) playerY += dy
   }
 
   /** Run the end game state */
   // function gameOver() {
-  //   clearCanvas();
-  //   drawEndText();
+  //   clearCanvas()
+  //   drawEndText()
   // }
 
   function drawPlayer() {
-    ctx.beginPath();
-    ctx.rect(playerX, playerY, PLAYER_SIZE, PLAYER_SIZE);
-    ctx.fillStyle = PLAYER_COLOR;
-    ctx.fill();
-    ctx.closePath();
+    ctx.beginPath()
+    ctx.rect(playerX, playerY, PLAYER_SIZE, PLAYER_SIZE)
+    ctx.fillStyle = COLORS.PLAYER_COLOR
+    ctx.fill()
+    ctx.closePath()
   }
 
   function level1() {
-    for (var b = 0; b < LEVEL_1_GEO.length; b++) {
-      var geo = LEVEL_1_GEO[b]
-      ctx.beginPath();
-      ctx.rect(geo.x, geo.y, geo.w, geo.h);
-      ctx.fillStyle = LEVEL_COLOR;
-      ctx.fill();
-      ctx.closePath();
+    for (var b = 0; b < LEVEL_1.geo.length; b++) {
+      var geo = LEVEL_1.geo[b]
+      ctx.beginPath()
+      ctx.rect(geo.x, geo.y, geo.w, geo.h)
+      ctx.fillStyle = COLORS.LEVEL_COLOR
+      ctx.fill()
+      ctx.closePath()
     }
   }
 
   function nega1() {
-    ctx.beginPath();
-    ctx.rect(240, canvas.height - canvas.height/4, PLAYER_SIZE, canvas.height/4);
-    ctx.fillStyle = ALT_LEVEL_COLOR;
-    ctx.fill();
-    ctx.closePath();
+    for (var b = 0; b < LEVEL_1.alt.length; b++) {
+      var alt = LEVEL_1.alt[b]
+      ctx.beginPath()
+      ctx.rect(alt.x, alt.y, alt.w, alt.h)
+      ctx.fillStyle = COLORS.ALT_LEVEL_COLOR
+      ctx.fill()
+      ctx.closePath()
+    }
   }
 
   function drawLevel() {
+    setBackgroundColor(ALT_WORLD ? COLORS.ALT_LEVEL_COLOR : null)
+
     if (ALT_WORLD) {
-      nega1()
-      /** Render alt level */
+      nega1() /** Render alt level */
     } else {
-      level1()
-      /** render main level */
+      level1() /** render main level */
+
+      /** draw exit */
+      ctx.beginPath()
+      ctx.rect(LEVEL_1.e.x, LEVEL_1.e.y, LEVEL_1.e.w, LEVEL_1.e.h)
+      ctx.fillStyle = COLORS.EXIT_COLOR
+      ctx.fill()
+      ctx.closePath()
     }
   }
 
   function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
   }
 
   function draw() {
@@ -179,8 +218,9 @@
     drawLevel()
     gameDebugger()
     canvasCollisionDetection()
+    exitCollisionDetection()
     requestAnimationFrame(draw)
   }
 
-  draw();
-})();
+  draw()
+})()
